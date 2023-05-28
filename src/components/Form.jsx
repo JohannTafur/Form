5440ce36a5fb61ef5d987829e9824b615a9cc92a
johann Tafur <johann012tafur@gmail.com>
2023-05-28 08:26:09 -0500

    Organized Form.jsx code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,115 +1,72 @@
 import React, { useState } from "react";
-import '../form.css'
-import profile from '../img/Adobe_20230525_234703 (1).png'
+import '../form.css';
+import profile from '../img/Adobe_20230525_234703 (1).png';
 
-const From = () => {
-
-    const [fastName, setFastName] = useState('');
-    const [lastName, setlastName] = useState('');
+const Form = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [password, setPassword] = useState('');
     const [contactNumber, setContactNumber] = useState('');
-    const [country, setCountry] = useState('')
-    const [city, setCity] = useState('')
-    const [state, setState] = useState('')
-    const [zipCode, setZipCode] = useState('')
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zipCode, setZipCode] = useState('');
+
+    const handleChange = (event, setStateFunction) => {
+        setStateFunction(event.target.value);
+    };
+
+    const validateText = (text) => /^[A-Za-z\s]+$/.test(text);
+    const validateNumbers = (number) => /^[0-9]+$/.test(number);
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        if (!validateText(fastName) || !validateText(lastName)) {
-            const fastNames = document.querySelector('.errorFastName');
-            fastNames.textContent = 'You can only enter letters in the first and last name.'
-        };
+        if (!validateText(firstName) || !validateText(lastName)) {
+            document.querySelector('.errorFastName').textContent = 'You can only enter letters in the first and last name.';
+        }
 
         if (password.length < 6 || !validateNumbers(password)) {
-            const passwords = document.querySelector('.errorPasword');
-            passwords.textContent = 'The password can only have numbers and be longer than 6 characters.'
+            document.querySelector('.errorPasword').textContent = 'The password can only have numbers and be longer than 6 characters.';
         }
 
         if (!validateNumbers(contactNumber)) {
-            const contactNumbers = document.querySelector('.errorContactNumber');
-            contactNumbers.textContent = 'The contact number can only have numbers.'
+            document.querySelector('.errorContactNumber').textContent = 'The contact number can only have numbers.';
         }
 
         if (!validateText(country)) {
-            const countrys = document.querySelector('.errorCountry')
-            countrys.textContent = 'Country supports text only A-Z'
+            document.querySelector('.errorCountry').textContent = 'Country supports text only A-Z.';
         }
 
         if (!validateText(city)) {
-            const citys = document.querySelector('.errorCity')
-            citys.textContent = 'City supports text only A-Z'
+            document.querySelector('.errorCity').textContent = 'City supports text only A-Z.';
         }
 
         if (!validateText(state)) {
-            const states = document.querySelector('.errorState')
-            states.textContent = 'State supports text only A-Z'
+            document.querySelector('.errorState').textContent = 'State supports text only A-Z.';
         }
 
         if (!validateNumbers(zipCode)) {
-            const zipCodes = document.querySelector('.errorZipCode')
-            zipCodes.textContent = 'The zip code only accepts numbers 0-9'
+            document.querySelector('.errorZipCode').textContent = 'The zip code only accepts numbers 0-9.';
         }
-    }
-
-    const handleFastNameChange = (event) => {
-        setFastName(event.target.value);
-    };
-
-    const handleLastNameChange = (event) => {
-        setlastName(event.target.value);
-    };
-
-    const validatePassword = (event) => {
-        setPassword(event.target.value);
-    }
-
-    const validateContactNumber = (event) => {
-        setContactNumber(event.target.value);
-    }
-
-    const validateCity = (event) => {
-        setCity(event.target.value)
-    }
-
-    const validateState = (event) => {
-        setState(event.target.value)
-    }
-
-    const validateZipCode = (event) => {
-        setZipCode(event.target.value)
-    }
-
-    const validateCountry = (event) => {
-        setCountry(event.target.value)
-    }
-
-    const validateNumbers = (number) => {
-        const numericPattern = /^[0-9]+$/;
-        return numericPattern.test(number)
-    }
-
-    const validateText = (text) => {
-        const pattern = /^[A-Za-z\s]+$/;
-        return pattern.test(text);
     };
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <h2>Edit Profil</h2>
+                <h2>Edit Profile</h2>
                 <div className="profile">
                     <img src={profile} alt="" className="profile-image" />
                 </div>
 
                 <div className="namesRow">
                     <div className="fastName">
-                        <label htmlFor="firstName">Fast Name</label>
-                        <input type="text" name="firstName" id="firstName" value={fastName} onChange={handleFastNameChange} />
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" name="firstName" id="firstName" value={firstName} onChange={(event) => handleChange(event, setFirstName)} />
                     </div>
                     <div className="lastName">
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" name="lastName" id="lastName" value={lastName} onChange={handleLastNameChange} />
+                        <input type="text" name="lastName" id="lastName" value={lastName} onChange={(event) => handleChange(event, setLastName)} />
                     </div>
                 </div>
                 <p className="errorFastName"></p>
@@ -118,7 +75,7 @@ const From = () => {
                 <input type="email" name="email" id="email" />
 
                 <label htmlFor="number">Contacts Number</label>
-                <input type="text" name="number" id="number" value={contactNumber} onChange={validateContactNumber} />
+                <input type="text" name="number" id="number" value={contactNumber} onChange={(event) => handleChange(event, setContactNumber)} />
                 <p className="errorContactNumber"></p>
 
                 <label htmlFor="address">Address</label>
@@ -127,32 +84,32 @@ const From = () => {
                 <div className="locationRow">
                     <div className="right">
                         <label htmlFor="country">Country</label>
-                        <input type="text" name="country" id="country" value={country} onChange={validateCountry} />
+                        <input type="text" name="country" id="country" value={country} onChange={(event) => handleChange(event, setCountry)} />
                         <p className="errorCountry"></p>
 
                         <label htmlFor="state">State</label>
-                        <input type="text" name="state" id="state" value={state} onChange={validateState} />
+                        <input type="text" name="state" id="state" value={state} onChange={(event) => handleChange(event, setState)} />
                         <p className="errorState"></p>
                     </div>
                     <div className="left">
                         <label htmlFor="city">City</label>
-                        <input type="text" name="city" id="city" value={city} onChange={validateCity} />
+                        <input type="text" name="city" id="city" value={city} onChange={(event) => handleChange(event, setCity)} />
                         <p className="errorCity"></p>
 
                         <label htmlFor="zipCode">Zip Code</label>
-                        <input type="text" name="zipCode" id="zipCode" value={zipCode} onChange={validateZipCode} />
+                        <input type="text" name="zipCode" id="zipCode" value={zipCode} onChange={(event) => handleChange(event, setZipCode)} />
                         <p className="errorZipCode"></p>
                     </div>
                 </div>
 
                 <label htmlFor="password">Password</label>
-                <input type="text" name="password" id="password" value={password} onChange={validatePassword} />
+                <input type="text" name="password" id="password" value={password} onChange={(event) => handleChange(event, setPassword)} />
                 <p className="errorPasword"></p>
 
                 <input type="submit" name="enviar" id="enviar" />
             </form>
         </>
-    )
-}
+    );
+};
 
-export default From
+export default Form;
